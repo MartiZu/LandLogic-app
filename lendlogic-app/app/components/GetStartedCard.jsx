f83c1cb1986ad3b042488d736eb96923b0224af0
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-12-05 16:07:58 +0000

    Home page designing

--- lendlogic-app/app/components/GetStartedCard.jsx
+++ lendlogic-app/app/components/GetStartedCard.jsx
@@ -1,6 +1,6 @@
 export default function GetStartedCard() {
   return (
-    <div className="card-container">
+    <div className="card-container mt-32 mx-4 mb-4">
       <h1>
         Welcome to LendLogic! We're not just about rates; we're about you.
       </h1>
@@ -9,7 +9,7 @@ export default function GetStartedCard() {
         offering personalized advice and finding competitive mortgage rates that
         fit your unique story.
       </p>
-      <button>Let's get started</button>
+      <button>Let's get started!</button>
     </div>
   );
 }
--- lendlogic-app/app/questionnaire/page.jsx
+++ lendlogic-app/app/questionnaire/page.jsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main>
       <GetStartedCard />
-      <div className="progress-circle-container">
-        <div className="current-circle"></div>
-        <div className="other-circle"></div>
-        <div className="other-circle"></div>
+      <div className="progress-circle-container flex flex-row justify-center items-center">
+        <div className="current-circle mx-1"></div>
+        <div className="other-circle mx-1"></div>
+        <div className="other-circle mx-1"></div>
       </div>
       <img />
     </main>
