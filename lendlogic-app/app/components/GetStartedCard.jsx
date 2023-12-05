38fc5bb36a0c456fc332868d1ee1ae2a44eeb176
MartiZu <martina.zurli@gmail.com>
2023-12-05 18:09:24 +0000

    disable eslint from getstartedcard

--- lendlogic-app/app/components/GetStartedCard.jsx
+++ lendlogic-app/app/components/GetStartedCard.jsx
@@ -2,13 +2,16 @@ export default function GetStartedCard() {
   return (
     <div className="card-container mt-32 mx-4 mb-4">
       <h1>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         Welcome to LendLogic! We're not just about rates; we're about you.
       </h1>
       <p>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         We're here to guide you through the exciting world of homeownership,
         offering personalized advice and finding competitive mortgage rates that
         fit your unique story.
       </p>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       <button>Let's get started!</button>
     </div>
   );
