cca9bdcf1b544213f2c84e2c415cab6bf445a4d0
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-12-06 15:57:17 +0000

    troubleshooting

--- lendlogic-app/app/dashboard/page.jsx
+++ lendlogic-app/app/dashboard/page.jsx
@@ -1,9 +1,11 @@
 import Newsletter from "../components/Newsletter"
 import LearningSection from "../components/LearningSection"
+import RemortgageReport from "../components/RemortgageReport"
 
 export default function Dashboard() {
   return (
     <div>
+      <RemortgageReport />
       <LearningSection />
     </div>
   )
