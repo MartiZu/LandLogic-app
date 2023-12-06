63c80b95401bde57dacb0f349930acb49ce3d0dd
MartiZu <martina.zurli@gmail.com>
2023-12-06 12:34:06 +0000

    questionnaire page logic

--- lendlogic-app/app/components/PreferenceTool.jsx
+++ lendlogic-app/app/components/PreferenceTool.jsx
@@ -2,18 +2,25 @@ import Link from "next/link";
 
 export default function PreferenceTool() {
   return (
-    <div className="flex flex-col items-center">
-      <form className="flex flex-col bg-slate-600 my-5">
-        <h2>What are you looking for</h2>
-        <input value="New Buyer" type="submit"></input>
-        <input value="Remortgage" type="submit"></input>
-        <input value="Moving House" type="submit"></input>
-        <input value="Just Browsing" type="submit"></input>
-      </form>
-      {/* <p>Repay your mortgage quicker</p>
+    <>
+      <div className="flex flex-col mt-32 mx-4 mb-4 bg-off-white  rounded-3xl p-3 shadow-card text-center text-2xl">
+        <form className="flex flex-col my-5">
+          <h2>What are you looking for</h2>
+          <input className="shadow-card rounded-3xl" value="New Buyer" type="submit"></input>
+          <input value="Remortgage" type="submit"></input>
+          <input value="Moving House" type="submit"></input>
+          <input value="Just Browsing" type="submit"></input>
+        </form>
+        {/* <p>Repay your mortgage quicker</p>
       <p>Reduce your monthly payments</p>
       <p>Change mortgage type</p>
       <p>Release equity</p> */}
-    </div>
+      </div>
+      <div className="flex flex-row justify-center items-center">
+        <div className="current-circle w-2 h-2 rounded-full bg-off-white shadow-card mx-1"></div>
+        <div className="w-3 h-3 rounded-full bg-purple-300 shadow-card mx-1"></div>
+        <div className="w-2 h-2 rounded-full bg-off-white shadow-card mx-1"></div>
+      </div>
+    </>
   );
 }
