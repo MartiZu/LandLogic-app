091eb4c081739a37b8b84e5624666b76d7f92244
MartiZu <martina.zurli@gmail.com>
2023-12-06 11:39:02 +0000

    added eslint comments

--- lendlogic-app/app/components/GetStartedCard.jsx
+++ lendlogic-app/app/components/GetStartedCard.jsx
@@ -2,16 +2,18 @@ export default function GetStartedCard() {
   return (
     <div className="mt-32 mx-4 mb-4 bg-off-white  rounded-3xl p-3 shadow-card text-center text-2xl">
       <h1 className="font-extrabold py-7">
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         Welcome to LendLogic! We're not just about rates; we're about you.
       </h1>
       <p className="py-2 font-normal">
-
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         We're here to guide you through the exciting world of homeownership,
         offering personalized advice and finding competitive mortgage rates that
         fit your unique story.
       </p>
 
       <button className="w-48 h-16 bg-purple-accent  m-5 rounded-full text-xl text-off-white font-semibold shadow-button">
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         Let's get started!
       </button>
     </div>
