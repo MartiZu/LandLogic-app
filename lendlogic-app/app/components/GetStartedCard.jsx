af4444b2dfc8afa8874e2e6e241297e271e6a53e
Adam Hale <adam.hale92@googlemail.com>
2023-12-06 10:30:56 +0000

    added custom tailwind colors and converted a large portion of CSS into Tailwind

--- lendlogic-app/app/components/GetStartedCard.jsx
+++ lendlogic-app/app/components/GetStartedCard.jsx
@@ -1,15 +1,15 @@
 export default function GetStartedCard() {
   return (
-    <div className="card-container mt-32 mx-4 mb-4">
-      <h1>
+    <div className="mt-32 mx-4 mb-4 bg-off-white  rounded-3xl p-3 shadow-purple text-center text-2xl">
+      <h1 className="font-extrabold py-7">
         Welcome to LendLogic! We're not just about rates; we're about you.
       </h1>
-      <p>
+      <p className="py-2 font-normal">
         We're here to guide you through the exciting world of homeownership,
         offering personalized advice and finding competitive mortgage rates that
         fit your unique story.
       </p>
-      <button>Let's get started!</button>
+      <button className="w-48 h-16 bg-purple-accent shadow-small-purple m-5 rounded-full text-xl text-off-white font-semibold">Let's get started!</button>
     </div>
   );
 }
