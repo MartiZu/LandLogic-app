140309fb66d8864bee1c7bc1b31f004e9b1e892f
Adam Hale <adam.hale92@googlemail.com>
2023-12-07 16:50:46 +0000

    updated to allow for data changes

--- lendlogic-app/app/dashboard/DisplayMortgageInformation.jsx
+++ lendlogic-app/app/dashboard/DisplayMortgageInformation.jsx
@@ -13,16 +13,16 @@ export default function DisplayMortgageInformation({ value }) {
   const [monthlyPayment, setMonthlyPayment] = useState(userMonthlyPayment);
   const [interestRate, setInterestRate] = useState(userInterestRate);
   const [loanTerm, setLoanTerm] = useState(loanLength);
-  
-  const updateMonthlyPayment = (e) => {
-    setInterestRate(e.target.value)
-    setLoanTerm(e.target.value)
-    setMonthlyPayment(loanAmount/(loanTerm*12))
-  }
+
+  const updateMonthlyPayment = () => {
+    const newMonthlyPayment = loanAmount / (loanTerm * 12);
+    setMonthlyPayment(newMonthlyPayment);
+    console.log(monthlyPayment);
+  };
   return (
     <div className="mt-32 mx-4 text-center text-2xl">
-      <div>Your new monthly payment could be {monthlyPayment}</div>
-      <div>Your remaining balance is {loanAmount}</div>
+      <div>Your new monthly payment could be £{monthlyPayment}</div>
+      <div>Your remaining balance is £{loanAmount}</div>
       <div id="toggles" className="flex flex-col">
         <label htmlFor="interestRate">Interest Rate: {interestRate}%</label>
         <input
@@ -31,7 +31,10 @@ export default function DisplayMortgageInformation({ value }) {
           max="9"
           step="0.1"
           value={interestRate}
-          onChange={(e) => updateMonthlyPayment}
+          onChange={(e) => {
+            setInterestRate(e.target.value);
+            updateMonthlyPayment;
+          }}
         />
 
         <label htmlFor="loanTerm">Loan Term: {loanTerm}</label>
@@ -42,7 +45,10 @@ export default function DisplayMortgageInformation({ value }) {
           max="35"
           step="1"
           value={loanTerm}
-          onChange={updateMonthlyPayment}
+          onChange={(e) => {
+            setLoanTerm(e.target.value);
+            updateMonthlyPayment;
+          }}
         />
       </div>
     </div>
