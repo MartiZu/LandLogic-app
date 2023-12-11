ec72edcffdaa7875fdb58017ece6c40ce1c661ce
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-12-11 16:04:55 +0000

    Display deposit and button

--- lendlogic-app/app/customHooks/DisplayKat.jsx
+++ lendlogic-app/app/customHooks/DisplayKat.jsx
@@ -7,4 +7,17 @@ export default async function DisplayKat() {
 
   const kat = userData.find(
     (user) => user.email === "kat.johnson@example.com"
-  )};
+  );
+
+  if (kat.email === "kat.johnson@example.com") {
+    const salary = kat.salary;
+    const property_value = kat.property_value;
+    const credit_score = kat.credit_score;
+
+    console.log(salary, property_value, credit_score);
+
+    return { salary, property_value, credit_score };
+  }
+
+  console.log("no user found");
+}
--- lendlogic-app/app/dashboard/page.jsx
+++ lendlogic-app/app/dashboard/page.jsx
@@ -1,17 +1,22 @@
 import LearningSection from "./LearningSection";
 import Newsletter from "./Newsletter";
-import RemortgageReport from "./RemortgageReport";
+// import RemortgageReport from "./RemortgageReport";
 import DisplayJenny from "../customHooks/DisplayJenny";
+import DisplayKat from "../customHooks/DisplayKat";
+import DepositButton from "./DepositButton";
 
 export default async function Dashboard() {
   //destructing the object returned from the custom hook
   const jenny = await DisplayJenny();
   console.log(jenny);
 
+  const kat = await DisplayKat();
+  console.log(kat);
+
   return (
     <div className="my-8 rounded-3xl text-center text-2xl">
       <h1 className="font-extrabold pt-7 text-5xl">Welcome Jenny!</h1>
-      <RemortgageReport value={jenny} />
+      <DepositButton value={kat} />
       <LearningSection />
       <Newsletter />
     </div>
