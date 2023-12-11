d4baca9eca6285d39f2786097d6e2834b00887ff
horiaomar25 <horiaomar6@gmail.com>
2023-12-11 12:09:36 +0000

    update

--- lendlogic-app/app/dashboard/page.jsx
+++ lendlogic-app/app/dashboard/page.jsx
@@ -2,7 +2,7 @@ import LearningSection from "./LearningSection";
 import Newsletter from "./Newsletter";
 import RemortgageReport from "./RemortgageReport";
 import DisplayJenny from "../customHooks/DisplayJenny";
-import Checklist from "./Checklist";
+
 
 export default async function Dashboard() {
   //destructing the object returned from the custom hook
@@ -10,14 +10,15 @@ export default async function Dashboard() {
   console.log(jenny);
 
   return (
-    // <div className="my-8 rounded-3xl text-center text-2xl">
-    //   <h1 className="font-extrabold pt-7 text-5xl">Welcome Jenny!</h1>
-    //   <RemortgageReport value={jenny} />
-    //   <LearningSection />
-    //   <Newsletter />
-    // </div>
-    <div>
-      <Checklist/>
+    <>
+    <div className="my-8 rounded-3xl text-center text-2xl">
+      <h1 className="font-extrabold pt-7 text-5xl">Welcome Jenny!</h1>
+      <RemortgageReport value={jenny} />
+      <LearningSection />
+      <Newsletter />
     </div>
+    <div>
+    </div>
+    </>
   );
 }
