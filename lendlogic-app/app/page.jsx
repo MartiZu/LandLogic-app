1bcca6682658d1ac7f6df5109188e81323137d6f
Joe O'Donovan <jojoodonovan@hotmail.com>
2023-12-11 16:26:25 +0000

    checklistComplete

--- lendlogic-app/app/page.jsx
+++ lendlogic-app/app/page.jsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
+import Checklist from "./dashboard/Checklist";
+import ChecklistExpand from "./dashboard/ExpandChecklist";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center">
-        <div className="my-32 mx-4 w-1/2 bg-off-white rounded-3xl p-3 shadow-card text-center text-2xl hover:bg-purple-accent hover:text-off-white hover:font-semibold">
-          <Link className="font-extrabold py-7" href="/questionnaire">
-            Mortgage
-          </Link>
-        </div>
+    {/* //   <div className="flex flex-col items-center">
+    //     <div className="my-32 mx-4 w-1/2 bg-off-white rounded-3xl p-3 shadow-card text-center text-2xl hover:bg-purple-accent hover:text-off-white hover:font-semibold">
+    //       <Link className="font-extrabold py-7" href="/questionnaire">
+    //         Mortgage
+    //       </Link>
+    //     </div>
 
-        <Image loading="eager"
-          className=""
-            src={"/Logo_lendlogic.png"}
-          width={300}
-          height={500}
-          alt="LandLogic Logo"
-          priority={true}
-        />
-      </div>
+    //     <Image loading="eager" */}
+    {/* //       className=""
+    //         src={"/Logo_lendlogic.png"}
+    //       width={300}
+    //       height={500}
+    //       alt="LandLogic Logo"
+    //       priority={true}
+    //     />
+    //   </div> */}
+    <ChecklistExpand />
     </main>
   );
 }
