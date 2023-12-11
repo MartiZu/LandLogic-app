63c3ea0a9be6fa57859438aa4158126c33c9dd0c
horiaomar25 <horiaomar6@gmail.com>
2023-12-11 12:01:41 +0000

    checklist component

--- lendlogic-app/app/dashboard/page.jsx
+++ lendlogic-app/app/dashboard/page.jsx
@@ -2,6 +2,7 @@ import LearningSection from "./LearningSection";
 import Newsletter from "./Newsletter";
 import RemortgageReport from "./RemortgageReport";
 import DisplayJenny from "../customHooks/DisplayJenny";
+import Checklist from "./Checklist";
 
 export default async function Dashboard() {
   //destructing the object returned from the custom hook
@@ -9,11 +10,14 @@ export default async function Dashboard() {
   console.log(jenny);
 
   return (
-    <div className="my-8 rounded-3xl text-center text-2xl">
-      <h1 className="font-extrabold pt-7 text-5xl">Welcome Jenny!</h1>
-      <RemortgageReport value={jenny} />
-      <LearningSection />
-      <Newsletter />
+    // <div className="my-8 rounded-3xl text-center text-2xl">
+    //   <h1 className="font-extrabold pt-7 text-5xl">Welcome Jenny!</h1>
+    //   <RemortgageReport value={jenny} />
+    //   <LearningSection />
+    //   <Newsletter />
+    // </div>
+    <div>
+      <Checklist/>
     </div>
   );
 }
