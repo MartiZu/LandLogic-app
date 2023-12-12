59e1a5bba159ef9d8d687458cab48b09c5b9a3df
MartiZu <martina.zurli@gmail.com>
2023-12-12 18:00:34 +0000

    implemented loading suspense to deshboard

--- lendlogic-app/app/dashboard/page.jsx
+++ lendlogic-app/app/dashboard/page.jsx
@@ -6,11 +6,11 @@ import DisplayJenny from "../customHooks/DisplayJenny";
 import DisplayKat from "../customHooks/DisplayKat";
 import DepositTool from "./NewBuyerReport";
 import { Suspense } from "react";
-import Loading from "../Loading";
+import Loading from "../loading";
 import Image from "next/image";
 
-
 export default async function Dashboard() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   //destructing the object returned from the custom hook
   const jenny = await DisplayJenny();
   console.log(jenny);
@@ -19,14 +19,16 @@ export default async function Dashboard() {
   console.log(kat);
 
   return (
-   <Suspense fallback={<Loading />}>
+    <Suspense fallback={<Loading />}>
       <div className="flex flex-col my-8 rounded-3xl text-center text-2xl">
-        <h1 className="font-normal pt-7 text-3xl text-purple-accent">We've got your back!</h1>
+        <h1 className="font-normal pt-7 text-3xl text-purple-accent">
+          We've got your back!
+        </h1>
         <p className="px-2 py-4 text-lg">
           Welcome {kat.userName}, here is everything you need to know
         </p>
-{/*<RemortgageReport value={jenny} />*/}
-        <DepositTool value={kat} /> 
+        {/*<RemortgageReport value={jenny} />*/}
+        <DepositTool value={kat} />
         <LearningSection />
         <Newsletter />
       </div>
