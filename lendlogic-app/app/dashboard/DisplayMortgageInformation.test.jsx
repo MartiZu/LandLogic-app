e5db4e5959252b818f9614505804f3fb967b568b
MartiZu <martina.zurli@gmail.com>
2023-12-13 18:11:30 +0000

    added testing of displaying dashboard and tool

---
+++ lendlogic-app/app/dashboard/DisplayMortgageInformation.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe } from "node:test";
+import DisplayMortgageInformation from "./DisplayMortgageInformation";
+
+describe("DisplayMortgageInformation", () => {
+  describe("Render", () => {
+    it("should render the component", () => {
+      render(<DisplayMortgageInformation />);
+      expect(
+        screen.getByText("Your new monthly payment could be")
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe("Function", () => {
+    it("should update monthly payment when interest rate is changed", () => {
+      const mockUpdateMonthlyPayment = jest.fn();
+      const event = {
+        target: { value: "5" },
+      };
+      render(
+        <DisplayMortgageInformation
+          updateMonthlyPayment={mockUpdateMonthlyPayment}
+        />
+      );
+      const input = screen.getByTestId("interestRate");
+      //action
+      fireEvent.change(input, event);
+      //assertion
+      expect(mockUpdateMonthlyPayment).toHaveBeenCalled();
+    });
+  });
+});
