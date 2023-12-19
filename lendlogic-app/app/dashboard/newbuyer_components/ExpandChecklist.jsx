f2b38dceb2114fb1b7be83f7c7b365aca40f20ee
Adam Hale <adam.hale92@googlemail.com>
2023-12-19 12:22:06 +0000

    fixed button hover and button sizing

--- lendlogic-app/app/dashboard/newbuyer_components/ExpandChecklist.jsx
+++ lendlogic-app/app/dashboard/newbuyer_components/ExpandChecklist.jsx
@@ -3,42 +3,42 @@ export default function ExpandChecklist() {
     <>
       <a
         target="_blank"
-        className="bg-off-white text-lg font-normal flex items-center justify-center h-16 p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="bg-off-white text-lg font-normal flex items-center justify-center h-16 p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/government/publications/how-to-buy-a-home/how-to-buy"
       >
         How to Buy Guide
       </a>
       <a
         target="_blank"
-        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/first-homes-scheme"
       >
         First-time buyer schemes
       </a>
       <a
         target="_blank"
-        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/stamp-duty-land-tax"
       >
         Stamp Duty
       </a>
       <a
         target="_blank"
-        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/right-to-buy-buying-your-council-home"
       >
         Right to Buy
       </a>
       <a
         target="_blank"
-        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/lifetime-isa"
       >
         Lifetime ISA
       </a>
       <a
         target="_blank"
-        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button hover:bg-purple-accent hover:text-off-white hover:font-semibold"
         href="https://www.gov.uk/shared-ownership-scheme"
       >
         Shared ownership
