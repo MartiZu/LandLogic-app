87f795749e6b4cfb7f8dbcb11205f510bd18ca69
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-12-19 16:30:13 +0000

    getUsers.test.jsx

--- lendlogic-app/app/customHooks/DisplayProperties.test.jsx
+++ lendlogic-app/app/customHooks/DisplayProperties.test.jsx
@@ -1,14 +1,15 @@
 import { describe } from "node:test";
-import displayProperties from "./DisplayProperties";
-import GetProperties from "@/library/getProperties";
+import GetProperties from "./DisplayProperties";
+import getProperties from "../../library/getProperties";
 
 const mockData = [
     {
-        id: 1,
         postcode: "SW1A 1AA",
-        city: "London",
-        council: "City of Westminster",
-        value: "200000"
+        value: "200000",
+      },
+      {
+        postcode: "SW1A 1AA",
+        value: "200000",
       },
     ];
 
@@ -16,8 +17,8 @@ jest.mock("../../library/getProperties");
 
 describe("DisplayProperties", () => {
     it("check output postcode", async () => {
-        GetProperties.mockResolvedValue(mockData);
-        const result = await displayProperties("SW1A 1AA");
-        expect(result.value).toEqual("200000");
+        getProperties.mockResolvedValue(mockData);
+        const result = await GetProperties();
+        expect(result.property[0].searchValue).toBe("200000");
     });
 });
