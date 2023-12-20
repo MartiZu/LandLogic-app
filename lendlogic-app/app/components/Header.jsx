9c6ce199613c9cf73ddffd6cc7687b1ff2348ca7
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-12-20 14:27:51 +0000

    troubleshooting

--- lendlogic-app/app/components/Header.jsx
+++ lendlogic-app/app/components/Header.jsx
@@ -1,21 +1,30 @@
 // import { FaRegUserCircle } from "react-icons/fa";
 import logo from "../../public/Icon.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <main>
       <header className="flex flex-row justify-between px-5 py-3 items-center">
-        <div className="flex flex-col justify-center">
-          <Image
-            src="/Logo_lendlogic.png"
-            width={100}
-            height={100}
-            alt="LandLogic Logo"
-            priority={true}
-            className="rounded-2xl"
-          />
+        <div className="flex flex-row justify-around items-center">
+          <Link href="/">
+            <Image
+              src="/Logo_lendlogic.png"
+              width={100}
+              height={100}
+              alt="LandLogic Logo"
+              priority={true}
+              className="rounded-2xl"
+            />
+          </Link>
+          <Link href="/questionnaire">
+        <div className="flex flex-col items-center text-purple-accent h-2 text-xl">
+          <h1>Retake the questionnaire</h1>
         </div>
+        </Link>
+        </div>
+        
         <div className="flex flex-col items-center text-purple-accent h-2 font-semibold text-xl">
           <h1>LendLogic</h1>
         </div>
