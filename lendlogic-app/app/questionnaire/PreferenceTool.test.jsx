f44c1c772890c310f7f850a3e9a7ec0d2e9ae92d
Chazzy11 <charlotterogers96@hotmail.com>
2023-12-21 16:54:19 +0000

    working on preferencetool userouter issues

--- lendlogic-app/app/questionnaire/PreferenceTool.test.jsx
+++ lendlogic-app/app/questionnaire/PreferenceTool.test.jsx
@@ -1,13 +1,22 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 //userEvent is an alternative to fireEvent
-import { describe, it } from "node:test";
+import { describe } from "node:test";
 import PreferenceTool from "./PreferenceTool";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
+
+// Mock the next/router module
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
 
 describe("PreferenceTool", () => {
+  describe("Render", () => {
   it("renders the PreferenceTool page", () => {
     //arrange
-    render(<PreferenceTool />);
+    render(<PreferenceTool />)
+    expect(screen.getByText("Which of the following best describe you?")).toBeInTheDocument();
   });
 });
+}); 
