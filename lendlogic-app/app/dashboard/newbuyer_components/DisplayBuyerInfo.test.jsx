f2ed465b83c291c48357b656b9dd6aea11224458
horiaomar25 <horiaomar6@gmail.com>
2023-12-22 15:24:31 +0000

    test

---
+++ lendlogic-app/app/dashboard/newbuyer_components/DisplayBuyerInfo.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe } from "node:test";
+import DisplayNewBuyerInformation from "./DisplayNewBuyerInformation";
+
+jest.mock("../../customHooks/DisplayUser", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+const mockValue = {
+propertyValue: 100000,
+monthlySaving: 500,
+years: 5,
+months: 0,
+};
+
+describe("NewBuyer", () => {
+  describe("Render", () => {
+    it("should render the component", () => {
+      render(<DisplayNewBuyerInformation value={mockValue} />);
+      expect(
+        screen.getByLabelText(" Update the property value:")
+      ).toBeInTheDocument();
+    });
+  });
+});
